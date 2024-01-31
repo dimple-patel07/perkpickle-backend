0dabced5df740d55a2a63661f149d93346c9b3d7
geitvand-007 <eitvand@gmail.com>
2024-02-01 00:41:32 +0530

    reset contact mailer

--- mailer/contact_mailer.js
+++ mailer/contact_mailer.js
@@ -1,27 +1,26 @@
 const { mailerConfig } = require("./mailer_config");
 
 function sendContactMail(req, res) {
-	const subject = "Verify your email address";
-	const htmlTemplate = contactEmailTemplate(otp);
+	params = req.body;
+	const subject = "Contact details";
+	const htmlTemplate = contactEmailTemplate(params);
 
-	mailerConfig(email, subject, htmlTemplate);
+	mailerConfig(process.env.MAILER_EMAIL, subject, htmlTemplate);
 	res.statusCode = 200;
 	return { message: "contact email sent successfully" };
 }
 
-function contactEmailTemplate(otp) {
-	const orgName = "PerkPickle";
+function contactEmailTemplate(params) {
 	const currentTime = Date.now();
 	return `
     <html>
       <body>
       <!-- opacity: 0 - to ensures Gmail doesn't trim the email -->
       <span style="opacity: 0"> ${currentTime} </span>
-        <p>To finish setting up your ${orgName} account, we just need to make sure that this email address is yours.</p>
-        <p>To verify your email address, use this security code: <b>${otp}</b></p>
-        <p>If you didn't request this code, you can safely ignore this email. Someone else might have typed your email address by mistake.</p>
-        <p>Thanks,</p>
-        <p>The ${orgName} team</p>
+        <p>Name : ${params.name}</p>
+        <p>Email : ${params.email}</p>
+        <p>Subject : ${params.subject}</p>
+        <p>Message : ${params.message}</p>
         <!-- opacity: 0 - to ensure Gmail doesn't trim the email -->
         <span style="opacity: 0"> ${currentTime} </span>
       </body>
